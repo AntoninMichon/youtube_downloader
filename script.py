2e86acd68bbba56945d386f219a9e85d675faddd
AntoninMichon <antonin.michon@laposte.net>
2022-03-12 13:19:36 +0100

    Youtube Downlad

--- script.py
+++ script.py
@@ -1,19 +1,22 @@
 from pytube import YouTube      # pip install pytube
 
-# Demande du lien à l'utilisateur :
+# *Demande du lien à l'utilisateur :
 
 link = input("Entrer le lien de la vidéo que vous souhaitez télécharger :\n=>   ")
 yt = YouTube(link)
 
-#Showing details
+# *Détails :
+
 print("Titre: ",yt.title)
 print("Nombre de vu : ",yt.views)
 print("Durée de la vidéo: ",yt.length, " secondes")
 
-# Meilleur résolution
+# *Meilleur résolution:
+
 ys = yt.streams.get_highest_resolution()
 
-# Début téléchargement
+# *Début téléchargement :
+
 print("Téléchargement en cours...")
 ys.download()
 print("Téléchargement terminé !")
