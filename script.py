c50e563810f348ba40b2bebd0d0787a1aab06a26
AntoninMichon <antonin.michon@laposte.net>
2022-03-12 19:04:21 +0100

    Patch

--- script.py
+++ script.py
@@ -8,15 +8,20 @@ yt = YouTube(link)
 # *Détails :
 
 print("Titre: ",yt.title)
-print("Nombre de vu : ",yt.views)
+print("Nombre de vues : ",yt.views)
 print("Durée de la vidéo: ",yt.length, " secondes")
 
 # *Meilleur résolution:
 
 ys = yt.streams.get_highest_resolution()
 
+# *Pour lancer le téléchargement
+
+ask = input("Appuyer sur Entrer pour lancer le téléchargment")
+
 # *Début téléchargement :
 
 print("Téléchargement en cours...")
 ys.download()
 print("Téléchargement terminé !")
+print("La vidéo se trouve dans le dossier de ce script")
