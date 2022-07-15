3ade8b40edce7e631f955d23a6538237487261f0
AntoninMichon <antonin.michon@laposte.net>
2022-07-15 10:49:14 +0200

    Update new Script => Programm Reinforcement

--- script.py
+++ script.py
@@ -5,11 +5,17 @@ from pytube import YouTube
 def metadata(info):
     print("Title : {}\nViews : {}\nLength : {} secondes".format(info.title, info.views, info.length))
     ask = input("Do you want download this video ? [Y/n]\n=>").lower()
-    if ask == 'y':
-        return True
-    else :
-        print("You cancel operation.")
-        return False
+    ok = False
+    while not ok :
+        if ask == 'y':
+            ok = True
+            return True
+        elif ask == 'n':
+            ok = True
+            print("You cancel operation.")
+            return False
+        else :
+            ask = input("Do you want download this video ? [Y/n]\n=>").lower()
 
 def download(info, title):
     print("Downloading...")
@@ -17,10 +23,16 @@ def download(info, title):
     print("Download Done !")
     file_path = os.path.realpath(__file__)
     ask = input("Your Video is in {} d you want open it ? [Y/n]\n=>".format(os.getcwd())).lower()
-    if ask =='y':
-        os.startfile(str(title.title)+".mp4")
-    else :
-        exit()
+    ok = False
+    while not ok :
+        if ask =='y':
+            ok = True
+            os.startfile(str(title.title)+".mp4")
+        elif ask =='n':
+            ok = True
+            exit()
+        else :
+            ask = input("Your Video is in {} d you want open it ? [Y/n]\n=>".format(os.getcwd())).lower()
 
 def main():
     link = input("Enter your YouTube video link\n=>")
