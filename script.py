3eb22723f9cf85e25632724f4077f115ff42d69f
HookSander <hooksander@HookSander-Kali.WORK>
2022-07-21 01:39:49 +0200

    Patch + Ajout de fonctionnalité

--- script.py
+++ script.py
@@ -1,41 +1,49 @@
 # HookSander
 import os
+import platform
 from pytube import YouTube
 
+device_os = platform.system()
+
 def metadata(info):
-    print("Title : {}\nViews : {}\nLength : {} secondes".format(info.title, info.views, info.length))
-    ask = input("Do you want download this video ? [Y/n]\n=>").lower()
+    print("Titre : {}\nVus : {}\nDurée : {} minutes".format(info.title, info.views, info.length/60))
+    ask = input("Voulez-vous télécharher cette vidéo ? [O/n]\n=>").lower()
     ok = False
     while not ok :
-        if ask == 'y':
+        if ask == 'o':
             ok = True
             return True
         elif ask == 'n':
             ok = True
-            print("You cancel operation.")
+            print("Vous avez annulé l'opération.")
             return False
         else :
-            ask = input("Do you want download this video ? [Y/n]\n=>").lower()
+            ask = input("Voulez-vous télécharher cette vidéo ? [O/n]\n=>").lower()
 
-def download(info, title):
-    print("Downloading...")
+def download(info, title, type_device):
+    print("Téléchargement en cours ...")
     info.download()
-    print("Download Done !")
+    print("Téléchargement terminé !")
     file_path = os.path.realpath(__file__)
-    ask = input("Your Video is in {} d you want open it ? [Y/n]\n=>".format(os.getcwd())).lower()
+    ask = input("Votre vidéo est dans {} voulez-vous l'ouvrir ? [O/n]\n=>".format(os.getcwd())).lower()
     ok = False
     while not ok :
-        if ask =='y':
+        if ask =='o':
             ok = True
-            os.startfile(str(title.title)+".mp4")
+            if type_device == "Windows" :
+                os.startfile(str(title.title)+".mp4")
+            else :
+                print("Désolé, cette fonctionnalité n'est pas encore disponilbe pour Linux et Mac OS")
+                vid = str(os.getcwd()+"/"+title.title+".mp4")
+                open (vid)
         elif ask =='n':
             ok = True
             exit()
         else :
-            ask = input("Your Video is in {} d you want open it ? [Y/n]\n=>".format(os.getcwd())).lower()
+            ask = input("Votre vidéo est dans {} voulez-vous l'ouvrir ? [O/n]\n=>".format(os.getcwd())).lower()
 
 def main():
-    link = input("Enter your YouTube video link\n=>")
+    link = input("Entrer le lien de votre vidéo YouTube\n=>")
     yt = YouTube(link)
     ys = yt.streams.get_highest_resolution()
     if metadata(yt) :
