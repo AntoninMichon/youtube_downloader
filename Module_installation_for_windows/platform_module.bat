f05d0c254e105148e892bc75ac86b7a76eda4971
HookSandeer <hooksander@proton.me>
2022-07-24 16:58:02 +0200

    Remove olf file

--- Module_installation_for_windows/platform_module.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Installing platform module...
-pip install platform
-echo If you have an error, it's maybe because platform module is already installed
-echo Press any key to continue
-pause>nul
