02818d2d6f183412d20f9746848c64e0c98e7b99
HookSandeer <hooksander@proton.me>
2022-07-24 16:53:00 +0200

    Update Start script for Linux

--- Launch_4_Linux.sh
+++ Launch_4_Linux.sh
@@ -7,5 +7,5 @@ if __name__ == '__main__' :
     file_path = os.path.realpath(__file__)
     print("Project by HookSander (https://github.com/HookSandeer)\nStarting python script ...")
     file_path = file_path[:-17]
-    current_script = str(file_path) + "script.py"
+    current_script = str(file_path) + "Output_File/script.py"
     os.system("python3 {}".format(current_script))
