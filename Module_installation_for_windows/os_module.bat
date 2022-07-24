c36e8d19c27e98fda9ea9189b715026adbc9531f
HookSandeer <hooksander@proton.me>
2022-07-23 23:59:30 -0700

    Add module installation script for WIndows

---
+++ Module_installation_for_windows/os_module.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Installing os module ...
+pip install os
+echo If you have an error, it's maybe because os module is already installed
+echo Press any key to continue
+pause>nul
---
+++ Module_installation_for_windows/pytube_module.bat
@@ -0,0 +1,6 @@
+@echo off
+
+echo Installing pytube module ...
+pip install pytube
+echo press any key to continue
+pause>nul
